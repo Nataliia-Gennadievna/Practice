7f29234717429e1a35e90265d36fce462416ee67
Nataliia-Gennadievna <nataliudinskaya@gmail.com>
2023-06-22 20:11:03 +0300

    Task4

---
+++ Task4/Laboratory work 5(task4)/src/com/company/Comparator.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Comparator
+{
+    public static int compare(int a, int b)
+    {
+        if (a>b)
+        {
+            return 1;
+        }
+        else if (a == b)
+        {
+            return 0;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
